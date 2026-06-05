c53fa85861ca364f765cccf927b38bbf68a66654
Danie Yang <fukui.yang.23@ucl.ac.uk>
2026-06-05 19:36:54 +0100

    Keep shell process when running python and node

--- rpi4b_setup/start_dash.sh
+++ rpi4b_setup/start_dash.sh
@@ -2,4 +2,4 @@
 source ~/.zshrc
 cd /home/delab/repos/optogrid-manager
 git pull
-exec node dashboard/server.js
+node dashboard/server.js
--- rpi4b_setup/start_og.sh
+++ rpi4b_setup/start_og.sh
@@ -3,4 +3,4 @@ source ~/.zshrc
 cd /home/delab/repos/optogrid-manager
 git pull
 source venv/bin/activate
-exec python3 headless_optogrid_backend.py
+python3 headless_optogrid_backend.py
