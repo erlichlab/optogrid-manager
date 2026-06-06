0f03d7295b4d0ce86f9651c76e93274c0338f6cf
Danie Yang <fukui.yang.23@ucl.ac.uk>
2026-06-06 14:49:17 +0100

    Echo instead of print_step

--- rpi4b_setup/start_og.sh
+++ rpi4b_setup/start_og.sh
@@ -7,7 +7,7 @@ git reset --hard stable-release-spain
 chmod +x ~/repos/optogrid-manager/rpi4b_setup/start_og.sh
 chmod +x ~/repos/optogrid-manager/rpi4b_setup/start_dash.sh
 source venv/bin/activate
-print_step "Enabling Bluetooth..."
+echo "Enabling Bluetooth..."
 sudo rfkill unblock bluetooth || print_warning "Bluetooth may not be fully configured"
 sudo systemctl start bluetooth || print_warning "Bluetooth may not be fully configured"
 bluetoothctl power on || print_warning "Bluetooth dongle is not plugged in"
