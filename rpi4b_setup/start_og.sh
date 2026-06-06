bbf30ce5230a7993fda6253f2f96a0c06fa6ef4d
Danie Yang <fukui.yang.23@ucl.ac.uk>
2026-06-06 14:32:45 +0100

    Configure git pull in auto-start script

--- rpi4b_setup/start_og.sh
+++ rpi4b_setup/start_og.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ~/.zshrc
 cd /home/delab/repos/optogrid-manager
-git pull
+git pull origin main
 git reset --hard stable-release-spain
 chmod +x ~/repos/optogrid-manager/rpi4b_setup/start_og.sh
 source venv/bin/activate
